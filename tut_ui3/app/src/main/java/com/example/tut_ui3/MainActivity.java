a5ee3fbb8c6986b6e80954a3edd7e829230aac3d
AB <ab@IIITD-AudioLabs-iMac.local>
2021-06-05 20:18:08 +0530

    Images

--- tut_ui3/app/src/main/java/com/example/tut_ui3/MainActivity.java
+++ tut_ui3/app/src/main/java/com/example/tut_ui3/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.tut_ui3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox cbSuits, cbBarry, cbSilval;
+
+    private RadioGroup rgMarital;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +28,75 @@ public class MainActivity extends AppCompatActivity {
         cbBarry = findViewById(R.id.cbBarry);
         cbSilval = findViewById(R.id.cbSilval);
 
+        rgMarital = findViewById(R.id.rgMarital);  // Radio group
+
+//        progressBar = findViewById(R.id.pbCircle);
+        progressBar = findViewById(R.id.pbHorizontal);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10; i++){
+                    progressBar.incrementProgressBy(10);
+                    SystemClock.sleep(500);
+                }
+            }
+        });
+        thread.start();
+
+
+        int checkedButton = rgMarital.getCheckedRadioButtonId();
+
+        switch(checkedButton) {
+            case R.id.rbMarried:
+                Toast.makeText(MainActivity.this, "MARRIED!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rbSingle:
+                Toast.makeText(MainActivity.this, "Single!!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rbRelation:
+                Toast.makeText(MainActivity.this, "In a relationship!", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        rgMarital.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rbMarried:
+                        Toast.makeText(MainActivity.this, "MARRIED!", Toast.LENGTH_SHORT).show();
+
+//                        progressBar.setVisibility(View.GONE);
+
+                        break;
+                    case R.id.rbSingle:
+                        Toast.makeText(MainActivity.this, "Single!!", Toast.LENGTH_SHORT).show();
+
+//                        progressBar.setVisibility(View.VISIBLE);
+
+                        break;
+                    case R.id.rbRelation:
+                        Toast.makeText(MainActivity.this, "In a relationship!", Toast.LENGTH_SHORT).show();
+
+//                        progressBar.setVisibility(View.GONE);
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+
 //        if (cbSuits.isChecked()){
 //            Toast.makeText(this, "watched suits", Toast.LENGTH_SHORT).show();
 //        }else{
 //            Toast.makeText(this, "need to watch", Toast.LENGTH_SHORT).show();
 //        }
 
-        cbSuits.setOnClickListener(new CompoundButton.OnCheckedChangeListener() {
+        cbSuits.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
